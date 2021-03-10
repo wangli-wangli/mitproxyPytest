423de3c54663db7caafcfd9aa6994706ab657a76
wangli <wangli@jyfwyun.cn>
2021-03-10 17:51:05 +0800

    删了多余文件，修改了项目名

--- pytest_test/test.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("cd pytest_test")
-#os.system('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"../allure-report/index.html')
-#os.system('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"D://PycharmProjects//mitmproxyProject//pytest_test//allure-report//index.html')
-os.system(r"D:\PycharmProjects\mitmproxyProject\pytest_test\allure-report\index.html")
